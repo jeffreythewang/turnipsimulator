d3aed5d3f54cadf70f973c2efc3f8d1d08b9648a
Jeff Wang <jeffreywang93@gmail.com>
2014-05-07 01:42:28 -0700

    Updated php

--- index.php
+++ index.php
@@ -7,10 +7,17 @@
 </head>
 <body>
 	<h1>Peach Turnip Simulator</h1>
-	<button onclick="generateTurnip()">Down B</button>
+	<button onclick="generateTurnip()">Pull</button>
 	<div id="result">
-		<img src="" id="result-img">
-		<p id="result-statement"></p>
+		<img src="img/blank.png" id="result-img">
+		<p id="result-statement">You have not pulled!</p>
+	</div>
+	<div id="log">
+		<p>Stitches: <span id="num-stitches">0</span></p>
+		<p>Pigs: <span id="num-pigs">0</span></p>
+		<p>Bombs: <span id="num-bombs">0</span></p>
+		<p>Light Sabers: <span id="num-lightsabers">0</span></p>
+		<b><p>Total Pulls: <span id="total-pulls">0</span></p></b>
 	</div>
 	<footer>
 		Formula sheet can be found <a href="http://i.imgur.com/FdSCz.png">here</a>
